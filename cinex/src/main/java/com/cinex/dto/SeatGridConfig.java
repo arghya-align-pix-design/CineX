41e948fdf177bdc9de3e8b722a17607ce77245ae
arghya-align-pix-design <paularghya2000@gmail.com>
2026-05-25 15:01:47 +0530

    added aisle and damaged seats in structure

--- cinex/src/main/java/com/cinex/dto/SeatGridConfig.java
+++ cinex/src/main/java/com/cinex/dto/SeatGridConfig.java
@@ -1,12 +1,15 @@
 package com.cinex.dto;
 
-import lombok.Data;
 import java.util.List;
+
+import lombok.Data;
 
 @Data
 public class SeatGridConfig {
     private int rows;
     private int columns;
     private List<String> seatCodes;
+    private List<String> damagedSeats;
     private List<String> unavailableSeats;
+    private List<Integer> aisles;
 }
