1af248e5f58cc8bfe62d5772b85303413c908903
arghya-align-pix-design <paularghya2000@gmail.com>
2026-05-30 20:29:56 +0530

    modifications as some sections were left

--- cinex/src/main/java/com/cinex/repository/SectionRepository.java
+++ cinex/src/main/java/com/cinex/repository/SectionRepository.java
@@ -5,9 +5,43 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cinex.entity.Section;
+//@Repository
+public interface SectionRepository extends JpaRepository<Section, Long> 
+{
+    //private EntityManager entityManager;
 
-public interface SectionRepository extends JpaRepository<Section, Long> {
+    // public List<Theater> findTheatersById(Long theaterId) {
+    //     String jpql = "SELECT t FROM Theater t WHERE t.id = :theaterId";
+    //     return entityManager.createQuery(jpql, Theater.class)
+    //             .setParameter("theaterId", theaterId)
+    //             .getResultList();
+    // }
+    
     List<Section> findByTheatreId(Long theatreId);
+    // @Query("select s from Section s WHERE s.theatre.id = ?1")
+    // List<Section> findByTheatreId(Long theatreId);
+
+    
     List<Section> findByTheatreIdAndIsActiveTrue(Long theatreId);
+    // @Query("SELECT s FROM Section s WHERE s.theatre.id = :theatreId AND s.isActive = true)")
+    // List<Section> findByTheatreIdAndIsActiveTrue(Long theatreId);
+    // public List<Section> findTheatreByIdAndIsActive(Long theatreId, boolean isActive ){
+    //     String jpql = "SELECT s FROM Section s WHERE s.theatre.id = :theatreId AND s.isActive = :isActive";
+    //     return entityManager.createQuery(jpql, Section.class)
+    //             .setParameter("theatreId", theatreId)
+    //             .setParameter("isActive", isActive)
+    //             .getResultList();
+    // }
+    
     boolean existsByTheatreIdAndName(Long theatreId, String name);
+    // @Query("SELECT COUNT(s) FROM Section s WHERE s.theatre.id = :theatreId AND s.name = :name")
+    // boolean existsByTheatreIdAndName(Long theatreId, String name);
+    // public boolean existsByTheatreIdAndName(Long theatreId, String name){
+    // String jpql = "SELECT COUNT(s) FROM Section s WHERE s.theatre.id = :theatreId AND s.name = :name";
+    // Long count = entityManager.createQuery(jpql, Long.class)
+    //         .setParameter("theatreId", theatreId)
+    //         .setParameter("name", name)
+    //         .getSingleResult();
+    // return count > 0;
+    // }
 }
